2abb1e4f9f35a637fa3147ebb731b2f18537a135
HappyTrailer <285.zubchenko@gmail.com>
2016-09-29 00:43:56 +0300

    +

---
+++ Assets/Skripts/Days/Lightning.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Lightning : MonoBehaviour {
+
+    public Transform tr;
+    public float speed;
+    private Light light;
+    private int hour;
+
+	void Start () {
+        light = gameObject.GetComponent<Light>();
+	}
+	
+	void Update () {
+        hour = DateTime.Now.Hour;
+        transform.RotateAround(tr.position, Vector3.down, speed);
+	}
+}

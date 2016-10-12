c36fa674dfefe61c6ef26d4444c710d0d916db83
HappyTrailer <285.zubchenko@gmail.com>
2016-10-12 20:30:56 +0300

    Clouds

---
+++ Assets/Skripts/Days/CloudsMove.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudsMove : MonoBehaviour {
+
+    public GameObject[] clouds;
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}

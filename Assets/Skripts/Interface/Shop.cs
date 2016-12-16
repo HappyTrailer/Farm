81a87ef4ae98daf20868fd67602dcf0194bee661
HappyTrailer <285.zubchenko@gmail.com>
2016-12-16 22:24:43 +0200

    Shop started

---
+++ Assets/Skripts/Interface/Shop.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Shop : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale -= new Vector3(0.1f, 0.1f, 0.1f);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        
+    }
+}
